8d6ee2c035f028d9c49fbf3f208281730a0773d6
Bruno Gusmão <brunohgv2@gmail.com>
2018-11-10 13:16:58 -0300

    Lista de deputados e detalhes

---
+++ js/controllers/list.js
@@ -0,0 +1,48 @@
+angular
+  .module('angularQuiz')
+  .controller('listCtrl', ListController)
+
+  function ListController ($scope, $http) {
+    var self = this
+    
+    $scope.Title = 'Meu titulo'
+
+    self.page = 1
+    API_URL = 'https://dadosabertos.camara.leg.br/api/v2/deputados?itens=20&pagina='
+    loadData(API_URL + self.page)
+    
+    function loadData(url) {
+      $http.get(url)
+      .then(res => {
+        $scope.deputados = res.data.dados
+        // console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+        alert(err.xhrStatus + ":" + err.statusText)
+      })
+    }
+
+    $scope.nextPage = function() {
+      // console.log('next')
+      self.page++
+      loadData(API_URL + self.page)
+    }
+    
+    $scope.prevPage = function() {
+      // console.log('prev')
+      self.page--
+      loadData(API_URL + self.page)
+    }
+
+    $scope.carregarDeputadoSelecionado = function (uri) {
+      $http.get(uri)
+        .then(res => {
+          $scope.deputadoSelecionado = res.data.dados
+          // console.log(res.data)
+        })
+        .catch(err => {
+          // console.log(err)
+        })
+    }
+  }
